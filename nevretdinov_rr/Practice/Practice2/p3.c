7a4cc8a53f47a41b12a6db3feafb3e9e7fbcb4c3
radmirnevretdinov <radmir.nev@mail.ru>
2018-10-15 15:04:04 +0300

    Practice3

---
+++ nevretdinov_rr/Practice/Practice2/p3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+void main()
+{
+    printf("vv");
+	int a[5];
+	int i;
+	for( i = 0; i < 5; i++)
+	   {
+		   scanf("%d", &a[i]);
+	   }
+	for(i = 0; i < 5; i++)
+	   {
+		   printf("%d", a[i]);
+	   }
+}
